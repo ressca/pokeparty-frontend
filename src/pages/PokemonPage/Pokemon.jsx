7864f4e72287ca115ac7e36bd097a9b8f22d8927
Jarp <j.sykuteraaa@gmail.com>
2025-11-25 14:30:15 +0100

    add header fix search

--- src/pages/PokemonPage/Pokemon.jsx
+++ src/pages/PokemonPage/Pokemon.jsx
@@ -1,8 +1,10 @@
 import './Pokemon.css';
 import PokemonInfo from '../../components/PokemonInfo/PokemonInfo';
+import Header from '../../components/header/Header';
 export default function Pokemon() {
     return (
         <div className='abc'>
+        <Header/>
         <PokemonInfo/>
         </div>
     );
