ed9ad8f1941c73848e83450e7099406c4f3a4ef9
Jarp <j.sykuteraaa@gmail.com>
2025-11-25 12:57:18 +0100

    Almost entire PokemonPage

---
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -0,0 +1,23 @@
+import "./PokemonCard.css";
+export default function PokemonCard(props) {
+    return (
+        <div className="main-card">
+            <img src="" alt="" />
+
+            <div className="type">
+                <h6 className="text-card">Type</h6>
+                <p className="text-card-data">TEST</p>
+            </div>
+
+            <div className="left-card"> 
+                <h6 className="text-card">Height</h6>
+                <p className="text-card-data">TEST</p>
+            </div>
+
+            <div className="right-card">
+                <h6 className="text-card">Weight</h6>
+                <p className="text-card-data">TEST</p>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/pages/PokemonPage/Pokemon.jsx
@@ -0,0 +1,8 @@
+import './Pokemon.css'
+import PokemonInfo from '../../PokemonInfo/PokemonInfo';
+export default function Pokemon() {
+    return (
+        
+        <PokemonInfo/>
+    );
+}
