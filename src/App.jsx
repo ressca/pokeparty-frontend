91a46f3de1a57485ec46cde5473cbaf7365aa1b6
SzymonGP <grothaszymon@gmail.com>
2025-11-25 13:03:29 +0100

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import MainPage from './pages/MainPage.jsx'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Pokemon from "./components/pages/PokemonPage/Pokemon";
+import MainPage from './pages/MainPage.jsx'
+
 
 function App() {
     const [count, setCount] = useState(0)
-
     return (
-        <>
-            <MainPage />
-        </>
-    )
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* <Route path="/Pokemon" element={<Pokemon />} /> */}
+      </Routes>
+    </Router>
+    );
 }
 
 export default App
