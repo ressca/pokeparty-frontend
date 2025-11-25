474b959d1a44d5562e8b08c71703822532de2f14
SzymonGP <grothaszymon@gmail.com>
2025-11-25 13:21:30 +0100

    fix Co-authored-by: Jarpekulo <j.sykuteraaa@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pokemon from "./components/pages/PokemonPage/Pokemon";
-
+import MainPage from './pages/MainPage.jsx'
 
 function App() {
     const [count, setCount] = useState(0)
     return (
     <Router>
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element={<MainPage></MainPage>}/>
         <Route path="/Pokemon" element={<Pokemon />} />
       </Routes>
     </Router>
