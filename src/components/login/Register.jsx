6c1b4f8f8770334e82376e93e61230cc8745fee2
SzymonGP <grothaszymon@gmail.com>
2025-11-25 19:55:21 +0100

    add Login and Register components

---
+++ src/components/login/Register.jsx
@@ -0,0 +1,21 @@
+import './login.css';
+
+export default function Register({ onClose, switchToLogin }) {
+  return (
+    <div className="loginOverlay">
+      <div className="loginForm">
+        <h2>Register</h2>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+
+        <button className='loginBtn'>Register</button>
+        <button className='closeBtn' onClick={onClose}>Close</button>
+        <div className="switchText">
+          Already have an account?{" "}
+          <span className="linkRegForm" onClick={switchToLogin}>Login</span>
+        </div>
+      </div>
+    </div>
+  );
+}
