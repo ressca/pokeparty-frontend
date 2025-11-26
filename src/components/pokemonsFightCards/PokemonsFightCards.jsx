4e3897f38449e3cda0e6eba12377da667f9cc7a8
ressca <c.m.poczta00@gmail.com>
2025-11-26 19:15:58 +0100

    finished pokemon fights cards

--- src/components/pokemonsFightCards/PokemonsFightCards.jsx
+++ src/components/pokemonsFightCards/PokemonsFightCards.jsx
@@ -1,14 +1,37 @@
 import './PokemonsFightCards.css';
-export default function PokemonsFightCards(props) {
+import { useEffect, useState } from 'react';
+
+export default function PokemonsFightCards({ pokemon_id }) {
+    const [pokemonData, setPokemonData] = useState(null);
+
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            try {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_id}`);
+                const data = await res.json();
+
+                setPokemonData({
+                    name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+                    types: data.types.map(t => t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1)),
+                    sprite: data.sprites.other['official-artwork'].front_default,
+                });
+            } catch (error) {
+                console.error('Error fetching pokemon data:', error);
+            }
+        };
+
+        fetchPokemonData();
+    }, [pokemon_id]);
+
     return (
         
         <div className='pokemonCard'>
             <div className='pokemonImageContainer'>
-                <img className='pokemonImage' src={props.img} alt='pokemon'/>
+                <img className='pokemonImage' src={pokemonData?.sprite} alt='pokemon'/>
             </div>
             <div className='pokemonInfoContainer'>
-                <div className='pokemonName'>{props.name}</div>
-                <div className='pokemonType'>Type: {props.type}</div>
+                <div className='pokemonName'>{pokemonData?.name}</div>
+                <div className='pokemonType'>Type: {pokemonData?.types.join(', ')}</div>
             </div>
         </div>
     )
