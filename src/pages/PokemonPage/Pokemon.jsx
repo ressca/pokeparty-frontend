82104ef00fc9052ad1efedb4bb8ad26c958dbd50
Jarp <j.sykuteraaa@gmail.com>
2025-11-26 10:25:08 +0100

    Color change

--- src/pages/PokemonPage/Pokemon.jsx
+++ src/pages/PokemonPage/Pokemon.jsx
@@ -1,11 +1,15 @@
 import './Pokemon.css';
 import PokemonInfo from '../../components/PokemonInfo/PokemonInfo';
 import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 export default function Pokemon() {
     return (
         <div className='abc'>
         <Header/>
         <PokemonInfo/>
+        <div className='def'>
+        <Footer/> 
+        </div>
         </div>
     );
 }
